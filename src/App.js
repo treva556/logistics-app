7b7b44665c57b579087bbfc6cdec17008b22e041
treva556 <marktreva92@gmail.com>
2024-07-30 00:24:46 +0300

    mile

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="">
+        logistics app
+      </header>
+      ships, planes , trucks & vehicles
+      smart storage solutions & containers
+      real time tracking website
+    </div>
+  );
+}
+
+export default App;
